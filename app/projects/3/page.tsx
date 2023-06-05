dfd0f8c095c61cafc31264fcff9313ce2984791c
Suraez <oberais50@gmail.com>
2023-06-05 21:29:28 +0545

    completed all projects layout

--- app/projects/3/page.tsx
+++ app/projects/3/page.tsx
@@ -1,9 +1,87 @@
+import Image from "next/image";
+import Link from "next/link";
 import React from "react";
 
 const NepaliNews = () => {
   return (
-    <div>
-      <h3 className="text-white">Edit your content here</h3>
+    <div className="container-fluid mt-24 p-4 md:p-24">
+      <div className="head">
+        <h3 className="text-white mb-4 md:text-4xl">
+          Automatic Speech Recognition System (ASR)
+        </h3>
+        <hr className="bg-white mb-4" />
+      </div>
+      <div className="body">
+        <div className="description">
+          <p className="mt-4 text-sm md:text-xl lg:text-2xl">
+            A DL model which accepts recorded audio as input and output the words spoken
+            in the audio along with the metrics such as Word Error Rate (WER) and
+            Character Error Rate (CER). We used transfer learning on the pre-trained model
+            provided by the Google - Transformers on the open source LibriSpeech Audio
+            Dataset which has approximately of 1000 hours and is sampled at the rate of
+            16KHz.
+          </p>
+          <div className="mt-4">
+            <Link
+              href="https://bookbytes.onrender.com/"
+              className="hover:cursor-pointer"
+              target="blank"
+            >
+              <h5 className="text-sm md:text-xl lg:text-2xl">
+                Demo URL: https://asr.streamlit.app/
+              </h5>
+            </Link>
+            <Link
+              href="https://github.com/Suraez/major-project"
+              className="hover:cursor-pointer"
+              target="blank"
+            >
+              <h4 className="text-sm md:text-xl lg:text-2xl">
+                Repo URL: https://github.com/Suraez/major-project
+              </h4>
+            </Link>{" "}
+          </div>
+        </div>
+
+        <div className="architecture md:mt-32">
+          <h3 className="text-white mb-4 mt-20 md:text-2xl">Outputs</h3>
+          <hr className="bg-white mb-4" />
+          <div className="output-section flex flex-col md:flex-row md:justify-around space-y-4">
+            <div className="first-col mt-4">
+              <Image src="/projects/speech.jpg" width="600" height="500" alt="speeech" />
+            </div>
+            <div className="second-col space-y-4">
+              <div className="card">
+                <h2 className="text-sm md:text-xl">Testing: </h2>
+                <Image
+                  src="/projects/output1.png"
+                  width="600"
+                  height="500"
+                  alt="output1"
+                />
+              </div>
+              <div className="card">
+                <h2 className="text-sm md:text-xl">Amplitude vs. Time: </h2>
+                <Image
+                  src="/projects/output2.png"
+                  width="600"
+                  height="500"
+                  alt="output2"
+                />
+              </div>
+              <div className="card">
+                <h2 className="text-sm md:text-xl">Spectogram: </h2>
+                <Image
+                  src="/projects/output3.png"
+                  width="600"
+                  height="500"
+                  alt="output3"
+                />
+              </div>
+            </div>
+          </div>
+        </div>
+      </div>
     </div>
   );
 };
--- app/projects/4/page.tsx
+++ app/projects/4/page.tsx
@@ -1,9 +1,72 @@
 import React from "react";
+import Image from "next/image";
+import Link from "next/link";
 
 const NepaliNews = () => {
   return (
-    <div>
-      <h3 className="text-white">Edit your content here</h3>
+    <div className="container-fluid mt-24 p-4 md:p-24">
+      <h3 className="text-white mb-4 md:text-4xl">
+        Mesla - A subtle clone of Stack Overflow
+      </h3>
+      <hr className="bg-white mb-4" />
+      {/* 1st row */}
+      <div className="flex xs:flex-col-reverse md:flex-row md:justify-around">
+        <div className="description md:w-3/5">
+          <p className="mt-4 text-sm md:text-xl lg:text-2xl">
+            A MERN (MONGODB + Express.js + React.js + NODE.js) web app which contains the
+            features such as authentication and authorization, upvote and downvoting
+            question and answers, question and answer posting, GitHub Job postings and so
+            on. The project was undertaken in order to help students scratching their head
+            during the time of exams to ask questions and get help from seniors, peers or
+            teachers. We also integrated a Nepal Online Payment System called Khalti for
+            fund-raising for distributing free books to the needy students.
+          </p>
+          <div className="mt-4">
+            <Link
+              href="https://bookbytes.onrender.com/"
+              className="hover:cursor-pointer"
+              target="blank"
+            >
+              <h5 className="text-sm md:text-xl lg:text-2xl">
+                Demo URL: https://bookbytes.onrender.com/
+              </h5>
+            </Link>
+            <Link
+              href="https://github.com/Suraez/bookbytes"
+              className="hover:cursor-pointer"
+              target="blank"
+            >
+              <h4 className="text-sm md:text-xl lg:text-2xl">
+                Repo URL: https://github.com/Suraez/bookbytes
+              </h4>
+            </Link>{" "}
+          </div>
+        </div>
+        <div className="demo md:w-1/5">
+          <Image src="/projects/mesla.jpg" alt="bookbytes" height="500" width="400" />
+        </div>
+      </div>
+
+      {/* 2nd row */}
+      <div className="container-fluid mt-10">
+        <div className="header flex flex-col space-y-2">
+          <h3 className="md:text-4xl">Technologies Used: </h3>
+          <hr className="bg-white w-full" />
+        </div>
+
+        <div className="flex flex-col items-center space-y-4 md:flex-row md:space-x-4 md:justify-around">
+          <Image src="/technologies/mongodb.png" height="100" width="250" alt="Node.js" />
+          <Image src="/technologies/express.jpg" height="100" width="150" alt="Node.js" />
+          <Image src="/technologies/react.jpg" height={100} width={150} alt="Node.js" />
+          <Image src="/technologies/nodej.png" height={100} width={150} alt="Node.js" />
+          <Image
+            src="/technologies/passport.png"
+            height="100"
+            width="250"
+            alt="Node.js"
+          />
+        </div>
+      </div>
     </div>
   );
 };
