6d8d0e6b0ff1864cb73a358a7bf5f940006db791
Suraez <oberais50@gmail.com>
2024-08-29 22:57:28 -0400

    feat: addded the editor feature

---
+++ app/editor/page.tsx
@@ -0,0 +1,18 @@
+'use client';
+import dynamic from "next/dynamic";
+import { useState } from "react";
+
+const Editor = dynamic(() => import("../components/editor"), { ssr: false });
+
+const page = () => {
+
+  const [data, setData] = useState('');
+
+  return (
+    <div>
+      <Editor value={data} onChange={setData} holder="editorjs-container" />
+    </div>
+  )
+}
+
+export default page;
